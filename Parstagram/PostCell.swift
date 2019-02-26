2a76bd2278a2c15e3a6178fae76dad7415708b0f
Nana Asubonteng <namankwah16@students.desu.edu@Nanas-MacBook-Pro.local>
2019-02-25 23:01:59 -0800

    first commit

---
+++ Parstagram/PostCell.swift
@@ -0,0 +1,33 @@
+//
+//  PostCell.swift
+//  Parstagram
+//
+//  Created by Nana Asubonteng on 2/24/19.
+//  Copyright © 2019 Nana Asubonteng. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var photoView: UIImageView!
+    
+    
+    @IBOutlet weak var usernameLabel: UILabel!
+    
+    
+    @IBOutlet weak var captionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
